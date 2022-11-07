c51e8aa6a3d7ad914cd5b9c3eb56e37b396f229f
Seongjae <cdaosldk@naver.com>
2022-11-07 22:46:00 +0900

    22/11/07

---
+++ src/com/codechobo/book/PackageTest.java
@@ -0,0 +1,8 @@
+package com.codechobo.book; // PackageTest 클래스가 속할 패키지의 선언
+
+public class PackageTest {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+}
